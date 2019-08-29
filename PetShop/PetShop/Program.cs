2f57bb29f0c6e47b8718dd792dcdb39d1dd207d9
Warfor <44639824+Warfor@users.noreply.github.com>
2019-08-29 11:27:21 +0200

    ll

---
+++ PetShop/PetShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsolePetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello diller!");
+        }
+    }
+}

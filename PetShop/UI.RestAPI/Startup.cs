916c2b82cae87399e43bcedfc9072a86c88cca08
Warfor <44639824+Warfor@users.noreply.github.com>
2019-09-03 17:10:16 +0200

    ne
    minifix

--- PetShop/UI.RestAPI/Startup.cs
+++ PetShop/UI.RestAPI/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.ApplicationService;
+using Core.DomainService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;

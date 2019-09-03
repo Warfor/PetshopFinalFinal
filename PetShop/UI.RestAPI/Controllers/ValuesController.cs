357d0ea493239e8a381155a7ac1405f574c698f3
Warfor <44639824+Warfor@users.noreply.github.com>
2019-09-03 17:06:29 +0200

    øv

--- PetShop/UI.RestAPI/Controllers/ValuesController.cs
+++ PetShop/UI.RestAPI/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace UI.RestAPI.Controllers
+namespace UI.RestApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]

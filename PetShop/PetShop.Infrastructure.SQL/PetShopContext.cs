e6ce3c45b89c3854bd234c8b1412dc29cfd8965f
Warfor <44639824+Warfor@users.noreply.github.com>
2019-09-11 11:00:54 +0200

    Nuget packages

--- PetShop/PetShop.Infrastructure.SQL/PetShopContext.cs
+++ PetShop/PetShop.Infrastructure.SQL/PetShopContext.cs
@@ -8,7 +8,16 @@ namespace PetShop.Infrastructure.SQL
 {
     public class PetShopContext : DbContext
     {
+        public PetShopContext(DbContextOptions opt)
+            : base(opt)
+        {
+
+        }
+
+
+
         public DbSet<Pet> Pets { get; set; }
         public DbSet<Owner> owners { get; set; }
     }
+
 }

ce1713854fb9afd975d264346ff9fadfcabf51a4
Warfor <44639824+Warfor@users.noreply.github.com>
2019-09-15 11:25:29 +0200

    Update OwnerRepository.cs

--- Petshop/Infrastructure/Repositories/OwnerRepository.cs
+++ Petshop/Infrastructure/Repositories/OwnerRepository.cs
@@ -22,7 +22,7 @@ namespace Infrastructure.Repositories
             return owner;
         }
 
-        public void Remove(Owner owner)
+        public Owner Remove(Owner owner)
         {
             Remove(owner);
         }

095d963c99ae61f97e25a910662c24ba705efa63
Warfor <44639824+Warfor@users.noreply.github.com>
2019-09-17 13:04:01 +0200

    Sql methods

--- PetShop/PetShop.Infrastructure.SQL/Repositories/OwnerSQLRepository.cs
+++ PetShop/PetShop.Infrastructure.SQL/Repositories/OwnerSQLRepository.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Core.DomainService;
+using Entity;
+using SQLitePCL;
 
 namespace PetShop.Infrastructure.SQL.Repositories
 {
-    class OwnerSQLRepository
+    class OwnerSQLRepository : IOwnerRepository
     {
+        private PetShopContext _context;
+        public OwnerSQLRepository(PetShopContext context)
+        {
+            _context = context;
+        }
+        public Owner Create(Owner owner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Owner Update(Owner ownerUpdate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Owner owner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Owner> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Owner FindOwnerById(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- PetShop/PetShop.Infrastructure.SQL/Repositories/PetSQLRepository.cs
+++ PetShop/PetShop.Infrastructure.SQL/Repositories/PetSQLRepository.cs
@@ -1,11 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Core.ApplicationService;
+using Core.DomainService;
+using Entity;
 
 namespace PetShop.Infrastructure.SQL
 {
-    class PetShopRepository
+    class PetSQLRepository : IPetRepository
     {
+        private PetShopContext _context;
 
+        public PetSQLRepository(PetShopContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Pet> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pet create(Pet pet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemovePet(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePet(Pet pet)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
